72ac91e4c512952432585d1bee9e4657f3cab7c6
wintanagb <wintanaltc@gmail.com>
2020-12-11 15:04:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function countDown(i)
+    {
+    let countDown = 10;
+    while (countDown > -1) {
+  console.log(countDown--);
+    }
+};
+
+const names = ["Ada", "Brendan", "Ali"]
+const event = 'surprise'
+ function writeCards(names,event)
+ {
+     let newArray = []
+    for (let i = 0; i < names.length; i++){
+        
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+ };
